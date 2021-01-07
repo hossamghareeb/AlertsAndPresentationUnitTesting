cd98ad7a1b471ec67ba8a504720bfbe4b4ee3a0a
Hossam Ghareeb <hossam.ghareb@gmail.com>
2021-01-07 13:03:03 +0100

    creating new project

---
+++ AlertsAndPresentationUnitTesting/AlertsAndPresentationUnitTesting/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AlertsAndPresentationUnitTesting
+//
+//  Created by Hossam Ghareeb on 07/01/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
